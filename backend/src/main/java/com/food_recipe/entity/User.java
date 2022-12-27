21f674a81291f7471e9828ee0b6c89f3e5ef7aed
HieuNB <hieunguyenk1098@gmail.com>
2022-12-27 23:46:44 +0700

    cm2

--- backend/src/main/java/com/food_recipe/entity/User.java
+++ backend/src/main/java/com/food_recipe/entity/User.java
@@ -16,7 +16,7 @@ public class User implements Serializable {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "`first_name`", nullable = false)
