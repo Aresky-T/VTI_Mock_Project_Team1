fa810ee2645d5fb859fb0dbdff450f54451dc993
Aresky-T <tn6354103@gmail.com>
2024-12-09 03:57:14 +0700

    backend: moved and renamed repository interface of Step entity

--- backend/src/main/java/com/food_recipe/entity/recipe/step/StepRepository.java
+++
@@ -1,6 +0,0 @@
-package com.food_recipe.entity.recipe.step;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StepRepository extends JpaRepository<Step, Integer> {
-}
