0e90e699cbd3cbcf9ffeba5cdb47e0132f8fa9cf
Aresky-T <tn6354103@gmail.com>
2024-12-08 22:36:46 +0700

    frontend: modified App.jsx: integrated newly created AppRouter component instead of Router components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import SignIn from "./components/auth/SignIn";
-import SignUp from "./components/auth/SignUp";
-import Profile from "./pages/Profile";
-import Settings from "./pages/Settings";
-import CreateRecipe2 from "./pages/CreateRecipe2";
-import Home2 from "./pages/Home2";
-import Footer from "./components/Footer";
-import RecipeDetail from "./pages/RecipeDetail";
-import UserInfo from "./components/ProfileComponent/UserInfo";
-import Account from "./components/ProfileComponent/Account";
-import RecipeManagement from "./components/ProfileComponent/RecipeManagement";
-import ExchangeHistory from "./components/ProfileComponent/ExchangeHistory";
-import { useSelector } from "react-redux";
-import ModalLogin from "./components/auth/ModalLogin";
-import Loading from "./components/Loading";
-import { Toaster } from "react-hot-toast";
+import React, { useCallback, useEffect } from "react";
+import AppRouter from "./router";
+import { useAuth } from "./redux/selector";
+import { signOutUser, validateAccessTokenApi } from "./api/auth.api";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
+  const auth = useAuth();
+  const currentUser = auth.signIn.currentUser;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-   const isShowPopup = useSelector(state => state.auth.signIn.isShowPopup);
-   const isLoading = useSelector(state => state.loading.isLoading);
+  const handleValidateUserAccessToken = useCallback(() => {
+    const token = currentUser?.token;
+    if (!token) return;
 
-   return (
-      <Router>
-         <Navbar />
-         <div className="container main">
-            <Routes>
-               {/* <Route path="/" element={<HomeComponent />} /> */}
-               <Route path="/" element={<Home2/>}/>
-               <Route path="/profile" element={<Profile/>}>
-                  <Route path="information" element={<UserInfo/>}/>
-                  <Route path="account" element={<Account/>}/>
-                  <Route path="recipes" element={<RecipeManagement/>}/>
-                  <Route path="exchange-history" element={<ExchangeHistory/>}/>
-               </Route>
-               <Route path="/create-recipe" element={<CreateRecipe2 />} />
-               <Route path="/settings" element={<Settings />} />
-               <Route path="/auth">
-                  <Route path="sign-in" element={<SignIn />} />
-                  <Route path="sign-up" element={<SignUp />} />
-               </Route>
-               <Route path="recipe/:id" element={<RecipeDetail/>}/>
-            </Routes>
-         </div>
-         <Footer/>
-         <Toaster
-            position="top center"
-         />
-         {isShowPopup && <ModalLogin/>}
-         {isLoading && <Loading isLoading={isLoading}/>}
-      </Router>
-   );
+    validateAccessTokenApi(token)
+      .then()
+      .catch(() => {
+        signOutUser(dispatch, navigate);
+      });
+  }, [currentUser, dispatch, navigate]);
+
+  useEffect(() => {
+    handleValidateUserAccessToken();
+  }, [handleValidateUserAccessToken]);
+
+  return <AppRouter />;
 };
 
 export default App;
