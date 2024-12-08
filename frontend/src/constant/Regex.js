3e4deea09010060ccee3c3f20c60bbf30cc4c2da
Aresky-T <tn6354103@gmail.com>
2024-12-08 22:04:34 +0700

    frontend: updated constant: new constant variable named ROUTES used for router

--- frontend/src/constant/Regex.js
+++ frontend/src/constant/Regex.js
@@ -1,4 +1,4 @@
-export const REGEX_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+export const REGEX_EMAIL = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
 export const REGEX_USERNAME = /^[A-Za-z0-9_-]{5,30}$/
 export const REGEX_PASSWORD = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/
-export const REGEX_PHONE = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+export const REGEX_PHONE = /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/
